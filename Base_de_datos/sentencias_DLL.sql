f541c71fb97965517ba21cc5d842e16a40e44d48
Samuel Quispe <proyectesis2004@gmail.com>
2025-10-26 18:27:29 -0500

    SQL

---
+++ Base_de_datos/sentencias_DLL.sql
@@ -0,0 +1,16 @@
+--- SENTENCIAS DDL ---
+--- CREATE TABLE TEMPLATE ---
+CREATE TABLE alumno(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    nro_documento varchar(15) NOT NULL,
+    nombre varchar(255) NOT NULL,
+    email varchar(255) NOT NULL
+);
+
+--- MODIFICAR UNA TABLA ---
+ALTER TABLE alumno 
+ADD COLUMN nota INT DEFAULT 0;
+
+--- ELIMINAR UNA TABLA ---
+DROP TABLE alumno;
+
