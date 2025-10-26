99b174f61d1a2e42aed21324a044f1a5fa90b3ad
Samuel Quispe <proyectesis2004@gmail.com>
2025-10-26 12:12:21 -0500

    Pruebas

--- Python/Funciones_archivos/01.Funciones.py
+++ Python/Funciones_archivos/01.Funciones.py
@@ -16,4 +16,4 @@ def sumar(a, b):
 num1 = float(input("Ingresa el primer número: "))
 num2 = float(input("Ingresa el segundo número: "))
 resultado_suma = sumar(num1, num2)
-print(f"La suma de {num1} y {num2} es: {resultado_suma}")   
+print(f"La suma de {num1} y {num2} es: {resultado_suma}")
